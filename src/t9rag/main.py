f404e429cb14c6f2386cc96a77ca974e8cc75064
Rune Hansen Steinnes-Westum <rune.steinnes@westum.no>
2025-04-04 12:02:44 +0200

    Started on synthetic training. 'rag train ..' will procuce training data. How to re-train a model is not yet implemented.

--- src/t9rag/main.py
+++ src/t9rag/main.py
@@ -15,9 +15,28 @@ from .embedding_model import EmbeddingModel, get_sentencetransformer
 from .ollama_llm import initialize_llm, stream_complete
 from .reranker import Document as FilteredDocument
 from .reranker import Reranker
+from .trainer import process_documents_with_questions
 from .vector_store import DocumentDict, VectorStore
 
 BASEDIR = Path(__file__).parent.resolve()
+
+
+class ProcessDocumentsOptions(TypedDict):
+    config: str
+    directory: Path
+    db_directory: str
+    chunk_size: int
+    chunk_overlap: int
+    llm_model: str
+    llm_base_url: str
+    llm_timeout: int
+    context_window: int
+    prompt: str
+    verbose: bool
+    llm_max_tokens: int
+    llm_temperature: float
+    llm_top_p: int
+    num_gpu: int
 
 
 class ReadDocumentsOptions(TypedDict):
@@ -52,7 +71,7 @@ class AskOptions(TypedDict):
     rerank_top_k: int
 
 
-OptionsType = TypeVar("OptionsType", bound=AskOptions | ReadDocumentsOptions)
+OptionsType = TypeVar("OptionsType", bound=AskOptions | ReadDocumentsOptions | ProcessDocumentsOptions)
 
 
 def load_config(config_file: str) -> dict[str, Any]:
@@ -123,6 +142,70 @@ class ConversationMemory:
 
         relevant_history = "\n".join([f"Q: {self.memory[i][0]}\nA: {self.memory[i][1]}" for i in top_indices])
         return f"Recent relevant conversation:\n{relevant_history}\n\n"
+
+
+@click.command()
+@click.option(
+    "--config",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    help="Path to the configuration file.",
+)
+@click.option(
+    "--prompt",
+    default="default",
+    help="The key to the prompt value stored in the configuration file",
+    show_default=True,
+)
+@click.option("--directory", default="./documents", help="Directory containing the documents", show_default=True)
+@click.option(
+    "--db-directory", default="./chroma_db", help="Directory where the vector database is stored", show_default=True
+)
+@click.option("--llm-model", default="llama3.2", help="Name of the Ollama LLM model", show_default=True)
+@click.option("--llm-base-url", default="http://localhost:11434", help="Base URL of the Ollama LLM", show_default=True)
+@click.option("--llm-timeout", default=600, help="Timeout for the Ollama LLM", show_default=True)
+@click.option("--context-window", default=3090, help="Context window size for Ollama LLM", show_default=True)
+@click.option("--verbose", is_flag=True, help="Enable verbose output", show_default=True)
+@click.option("--llm-max-tokens", default=1024, help="Maximum number of tokens for the Ollama LLM", show_default=True)
+@click.option("--llm-temperature", default=0.3, help="Temperature for the Ollama LLM", show_default=True)
+@click.option("--llm-top-p", default=0.5, help="Top-p for the Ollama LLM", show_default=True)
+@click.option("--num-gpu", default=0, help="Number of GPUs to use for Ollama LLM", show_default=True)
+@click.option("--chunk-size", default=1024, help="Size of document chunks", show_default=True)
+@click.option("--chunk-overlap", default=20, help="Overlap between chunks", show_default=True)
+@click.pass_context
+def process_documents(ctx: click.Context, **options: Unpack[ProcessDocumentsOptions]) -> None:
+    if options.get("config"):
+        # If config is passed in, the values in config take precedence over CLI options
+        # unless an option is explicitly provided
+        config, options = read_config(ctx, options, "train")
+    else:
+        config = {"prompts": {}}
+
+    _llm_options = {
+        "model_name": options["llm_model"],
+        "timeout": options["llm_timeout"],
+        "base_url": options["llm_base_url"],
+        "context_window": options["context_window"],
+        "verbose": options["verbose"],
+        "max_tokens": options["llm_max_tokens"],
+        "temperature": options["llm_temperature"],
+        "top_p": options["llm_top_p"],
+        "num_gpu": options["num_gpu"],
+    }
+
+    llm = initialize_llm(
+        **_llm_options,  # type: ignore[arg-type]
+    )
+
+    if llm is None:
+        click.secho("Failed to initialize Ollama LLM", fg="red")
+        return
+    _ = process_documents_with_questions(
+        options["directory"],
+        options["chunk_size"],
+        options["chunk_overlap"],
+        llm,
+        get_prompt(options["prompt"], config or {}),
+    )
 
 
 @click.command()
@@ -303,6 +386,7 @@ def cli() -> None:
     pass
 
 
+cli.add_command(process_documents, "train")
 cli.add_command(version)
 cli.add_command(read_documents)
 cli.add_command(ask)
---
+++ src/t9rag/trainer.py
@@ -0,0 +1,55 @@
+"""A simple training script for which I don't know the value of."""
+
+import json
+from contextlib import suppress
+from json.decoder import JSONDecodeError
+from pathlib import Path
+from typing import cast
+
+import click
+from llama_index.llms.ollama import Ollama
+
+from .document_reader import load_documents
+
+BASEDIR = Path(__file__).parent.parent.parent.resolve()
+
+
+def coerce_json(data: str) -> dict[str, str] | None:
+    doc = None
+    with suppress(JSONDecodeError):
+        doc = json.loads(data)
+
+    if doc is None:
+        # try to coerce data to JSON by checking for missing } in string
+        if "}" not in data:
+            data = f"{data}}}"
+        doc = json.loads(data)
+
+    return doc
+
+
+def generate_questions(llm: Ollama, chunk: str, prompt: str) -> dict[str, str] | None:
+    prompt = prompt.format(chunk=chunk)
+    response: str = cast("str", llm.complete(prompt))  # generate(prompt)
+    questions = coerce_json(str(response))
+    return questions
+
+
+def process_documents_with_questions(
+    directory: Path, chunk_size: int, chunk_overlap: int, llm: Ollama, prompt: str
+) -> None:
+    documents = load_documents(directory, chunk_size, chunk_overlap)
+    processed_documents = []
+    num = 0
+    for doc in documents:
+        questions = generate_questions(llm, doc["content"], prompt)
+        if questions is not None:
+            for key in questions:
+                processed_documents.append({key: questions[key], "chunk": doc["content"]})
+                num += 1
+        if num == 100:
+            break
+
+    with open(BASEDIR / "training.json", "w") as fp:
+        fp.write(json.dumps(processed_documents))
+        click.secho(f"Dumped {num} trainingdata for {num / 20} content chunks")
